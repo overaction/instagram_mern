e5c02ec6259a354535386104676e53e216fe4a38
overaction <52850392+overaction@users.noreply.github.com>
2020-12-03 12:59:23 +0900

    backend - 1. how to use middleware

--- server/app.js
+++ server/app.js
@@ -4,8 +4,21 @@ const app = express();
 
 const PORT = 5000;
 
+const customMiddleware = (req,res,next) => {
+    console.log("middleware");
+    next();
+}
+
+app.use(customMiddleware);
+
 app.get('/',(req,res) => {
+    console.log('home');
     res.send("hello!");
+});
+
+app.get('/about',customMiddleware,(req,res) => {
+    console.log('about');
+    res.send("about page!");
 });
 
 app.listen(PORT || 5000 , () => console.log(`server is running on ${PORT}`));
