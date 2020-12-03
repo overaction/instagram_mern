014eed8d5b4f1f6618faae31918940f33d1bc0b4
overaction <52850392+overaction@users.noreply.github.com>
2020-12-03 19:18:07 +0900

    backend - 4. saving or posting in Mongodb database

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+// saving or posting on mongodb
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
+
+
+
 router.get('/', (req,res) => {
     res.send("hello");
 });
 
 router.post('/signup', (req,res) => {
     const {name,email,password} = req.body;
+    console.log(email);
     if(!email || !password || !name) {
         return res.status(422).json({error: "please add all the fields"});
     }
-    res.status(200).json({message:"successfully posted"});
+    User.findOne({email:email}) // email 항목에 동일한 "email"이 있는지 찾아줌
+    .then((savedUser) => {
+        if(savedUser) {
+            return res.status(422).json({error: "user already exists with that email"});
+        }
+        else {
+            const user = new User({
+                name,
+                email,
+                password
+            });
+
+            user.save()
+            .then((user) => {
+                res.json({message: `saved successfully ${user}`})
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    })
+    .catch((err) => console.log(err));
 })
 
 module.exports = router;
