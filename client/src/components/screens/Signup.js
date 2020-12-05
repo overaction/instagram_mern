83e7d069102cf09a4aa38d8b3d3a2ec11e709161
overaction <52850392+overaction@users.noreply.github.com>
2020-12-05 12:46:57 +0900

    Frontend - 6. Signup by posting data
    Using toast
    and
    email address regular expression

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import './Sign.css'
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
+import M from 'materialize-css';
 const SignUp = () => {
+    const history = useHistory();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const PostData = async () => {
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+            return M.toast({html: "invalid email", classes:"#c62828 red darken-3"})
+        }
+        await fetch('/signup', {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                password,
+                email,
+            })
+        }).then(res => res.json())
+        .then((data) => {
+            if(data.error) {
+                M.toast({html: data.error, classes:"#c62828 red darken-3"})
+            }
+            else {
+                M.toast({html: data.message, classes:"#43a047 green darken-1"});
+                history.push('/signin');
+            }
+        })
+    }
+
     return (
         <div className="mycard">
             <div className="card auth-card">
                 <h2>Instagram</h2>
-                <input type="text" placeholder="name" />
-                <input type="text" placeholder="email" />
-                <input type="text" placeholder="password" />
+                <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)}/>
+                <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="text" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 <button
                     className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                    onClick={() => PostData()}
                 >
                     SignUp
                 </button>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -21,7 +21,7 @@ router.get('/protected', requireLogin, (req,res) => {
 
 router.post('/signup', (req,res) => {
     const {name,email,password} = req.body;
-    console.log(email);
+    console.log(req.body)
     if(!email || !password || !name) {
         return res.status(422).json({error: "please add all the fields"});
     }
